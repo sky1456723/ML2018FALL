e2bedc8a72c98864ff9970f6c251ae9dee381d1f
sky1456723 <b05901009@ntu.edu.tw>
2018-11-08 20:41:03 +0800

    Add PReLU model(66.870) and ensemble add this model

--- hw3/test.py
+++ hw3/test.py
@@ -29,6 +29,7 @@ model3 = keras.models.load_model("./model/lots_of_cnn_1.h5")
 model4 = keras.models.load_model("./model/lots_of_cnn_2.h5")
 model5 = keras.models.load_model("./model/lots_of_cnn_3.h5")
 model6 = keras.models.load_model("./model/lots_of_cnn_4.h5")
+model7 = keras.models.load_model("./model/my_cnn_PReLU.h5")
 print("Starting prediction")
 result1 = model1.predict(data)
 result2 = model2.predict(data)
@@ -36,10 +37,11 @@ result3 = model3.predict(data)
 result4 = model4.predict(data)
 result5 = model5.predict(data)
 result6 = model6.predict(data)
-result = (result1+result2+result3+result4+result5+result6)/6
+result7 = model7.predict(data)
+result = (result1+result2+result3+result4+result5+result6+result7)/7
 class_label = list(map(np.argmax,result))
 
-newFile = open("answer_ens_s01234.csv","w")
+newFile = open("answer_ens_7.csv","w")
 newFile.write("id,label\n")
 for i in range(len(class_label)):
     newFile.write(str(i)+","+str(class_label[i])+"\n")
