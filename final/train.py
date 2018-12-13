98062b9a09211bf294e5dc40388b5a3b55a7816c
sky1456723 <b05901009@ntu.edu.tw>
2018-12-13 11:33:29 +0800

    Fix bug in train.py

--- final/train.py
+++ final/train.py
@@ -35,12 +35,12 @@ for i in train_file.index:
         
 img_dirs = os.path.join(root_dir,"medical_images","images")
 
-#normalize = transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+normalize = transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
 transformList = []
-transformList.append(transforms.RandomResizedCrop(224))
+transformList.append(transforms.RandomResizedCrop(700))
 transformList.append(transforms.RandomHorizontalFlip())
-#transformList.append(transforms.ToTensor())
-#transformList.append(normalize)      
+transformList.append(transforms.ToTensor())
+transformList.append(normalize)      
 transformSequence=transforms.Compose(transformList)
 def get_dataloader(data_list, transform=None, normalize=None, batch_size = 4):
     part_data = []
@@ -52,20 +52,25 @@ def get_dataloader(data_list, transform=None, normalize=None, batch_size = 4):
         if transform != None:
             img = img.convert(mode="RGB")
             img = transform(img)
-            img = np.array(img)/255
-            img = np.transpose(img, axes=[2,0,1])
+            #img = np.array(img)/255
+            #img = np.transpose(img, axes=[2,0,1])
         else:
+            T = []
+            normalize = transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+            T.append(transforms.Resize(700))
+            T.append(transforms.ToTensor())
+            T.append(normalize)      
+            T=transforms.Compose(T)
+            
             img = img.convert(mode="RGB")
-            img = img.resize((224,224))
-            img = np.array(img) / 255
-            img = np.transpose(img, axes=[2,0,1])
+            img = T(img)
         label = pair[1].split()
         label = np.array([int(c) for c in label])
         part_label.append(label)
         part_data.append(img)
 
     batch = batch_size
-    label_data_x = torch.Tensor(part_data)
+    label_data_x = torch.stack(part_data)
     label_data_y = torch.Tensor(part_label)
     label_dataset = torch.utils.data.TensorDataset(label_data_x, label_data_y)
     label_dataloader = torch.utils.data.DataLoader(dataset = label_dataset,
@@ -82,7 +87,27 @@ def main(args):
         base_model.classifier = torch.nn.Linear(in_features = base_model.classifier.in_features,
                                                 out_features = 14,
                                                 bias = True)
-
+        base_model.add_module("pre_conv",
+                      torch.nn.Sequential(
+                          torch.nn.Conv2d(3,64, kernel_size=(8,8), stride = (1,1), padding = 1 ), #695x695
+                          torch.nn.BatchNorm2d(64),
+                          torch.nn.LeakyReLU(),
+                          torch.nn.MaxPool2d((3,3)), #231x231
+                          torch.nn.Dropout2d(0.3),
+                          torch.nn.Conv2d(64, 128, kernel_size=(5,5), stride=(1,1)),   #226x226
+                          torch.nn.BatchNorm2d(128),
+                          torch.nn.LeakyReLU(),
+                          torch.nn.Dropout2d(0.3),
+                          torch.nn.Conv2d(128, 256, kernel_size=(4,4), stride=(1,1),padding=1),   #224x224
+                          torch.nn.BatchNorm2d(256),
+                          torch.nn.LeakyReLU(),
+                          torch.nn.Dropout2d(0.3), 
+                          torch.nn.Conv2d(256, 3, kernel_size=(1,1), stride=(1,1)),   #224x224
+                          torch.nn.BatchNorm2d(3),
+                          torch.nn.LeakyReLU(),
+                          torch.nn.Dropout2d(0.3)
+                      )
+                     )
         base_model.add_module("output_act",torch.nn.Sigmoid())
         base_model = base_model.to(device)
         optimizer = torch.optim.Adam(base_model.parameters(),lr=0.001)
@@ -100,15 +125,16 @@ def main(args):
     train_data = label_data[len(label_data)//10:]
     val_data = label_data[:len(label_data)//10]
 
-
+    best_auroc = 0 
     for e in range(epoch):
         print("Epoch ",e)
         epoch_loss = 0
         epoch_acc = 0
-        for part in range(10):
+        
+        for part in range(4):
             gc.collect()
             print("Part ",part)
-            label_dataloader = get_dataloader(train_data[part*len(train_data)//10:(part+1)*len(train_data)//10],
+            label_dataloader = get_dataloader(train_data[part*len(train_data)//4:(part+1)*len(train_data)//4],
                                              transform = transformSequence,
                                              normalize = None,
                                              batch_size = args.batch_size)
@@ -118,7 +144,7 @@ def main(args):
                 label = label.to(device)
                 optimizer.zero_grad()
                 #preconv_optimizer.zero_grad()
-                pred = base_model.output_act( base_model(data) )
+                pred = base_model.output_act( base_model(base_model.pre_conv(data)) )
                 loss = criterion(pred,label)
                 loss.backward()
                 optimizer.step()
@@ -126,8 +152,7 @@ def main(args):
                 epoch_loss += loss.item()
                 epoch_acc += torch.sum(torch.eq((pred>0.5), label.byte())).item()/14
             del label_dataloader
-            torch.save(base_model, model_name)
-            torch.save(optimizer.state_dict(), model_name+".optim")
+        
         print("")
         print("Start Validation")
         val_loss = 0
@@ -139,13 +164,17 @@ def main(args):
             print("Batch: ", b_num, end='\r')
             data = data.to(device)
             label = label.to(device)
-            pred = base_model.output_act( base_model(data) )
+            pred = base_model.output_act( base_model(base_model.pre_conv(data)) )
             for one_row in pred.cpu().data.numpy():
                 ans_list.append(one_row)
             for one_row in label.cpu().data.numpy():
                 label_list.append(one_row)
         del val_dataloader
         auroc = sklearn.metrics.roc_auc_score(np.array(label_list),np.array(ans_list))
+        if auroc > best_auroc:
+            torch.save(base_model, model_name)
+            torch.save(optimizer.state_dict(), model_name+".optim")
+            best_auroc = auroc
         print("")
         print("Epoch loss: ",8*epoch_loss/(9*len(label_data)//10) )
         print("Epoch acc: ",epoch_acc/(9*len(label_data)//10) )
